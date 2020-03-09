bafb0fe7db56cfad237771585a8957c270094008
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-03-09 22:39:25 +0100

    configure security

---
+++ src/main/java/com/hydro17/spaceagencydatahub/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.hydro17.spaceagencydatahub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ContentManager").password("contentmanager").roles("CONTENT_MANAGER")
+                .and()
+                .withUser("Customer").password("customer").roles("CUSTOMER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/missions/**").hasRole("CONTENT_MANAGER")
+                .antMatchers("/products/**").hasAnyRole("CONTENT_MANAGER", "CUSTOMER")
+                .antMatchers("/h2-console/**").permitAll()
+                .and().httpBasic();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
