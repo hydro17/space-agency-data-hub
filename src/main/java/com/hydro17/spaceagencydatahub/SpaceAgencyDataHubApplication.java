7d6dc62f8ffa424a30f49b8ed3fbbf7fb0d14fec
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-03-09 20:55:37 +0100

    Initial commit

---
+++ src/main/java/com/hydro17/spaceagencydatahub/SpaceAgencyDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.hydro17.spaceagencydatahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAgencyDataHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceAgencyDataHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hydro17/spaceagencydatahub/SpaceAgencyDataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydro17.spaceagencydatahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceAgencyDataHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
