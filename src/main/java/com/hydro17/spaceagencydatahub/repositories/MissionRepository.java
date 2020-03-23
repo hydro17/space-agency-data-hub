5d5bdf16ace5f92b6230cdafc59bcabf4d47bec1
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-03-23 19:23:58 +0100

    add findByNameAndNotEqualId

--- src/main/java/com/hydro17/spaceagencydatahub/repositories/MissionRepository.java
+++ src/main/java/com/hydro17/spaceagencydatahub/repositories/MissionRepository.java
@@ -2,6 +2,7 @@ package com.hydro17.spaceagencydatahub.repositories;
 
 import com.hydro17.spaceagencydatahub.models.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     Optional<Mission> findByName(String name);
+
+    @Query("SELECT m FROM Mission m WHERE m.name = :name AND m.id <> :id")
+    Optional<Mission> findByNameAndNotEqualId(String name, Long id);
 }
